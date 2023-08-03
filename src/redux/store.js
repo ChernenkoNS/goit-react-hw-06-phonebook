18698bbbc47b46a6b7f5ea2eed455e7bc9d48c93
Nikita Chernenko <109902609+ChernenkoNS@users.noreply.github.com>
2023-08-03 16:20:56 +0300

    commit-five

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,37 @@
-import { configureStore } from '@reduxjs/toolkit';
-// import { contactFormReducer } from './contactFormReducer';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { phoneBookReducer } from './phoneBookReducer';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+const rootReducer = combineReducers({
+  phoneBook: phoneBookReducer,
+});
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    // contactForm: contactFormReducer,
-    phoneBook: phoneBookReducer
+  reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({ 
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }),
   },
-});
+);
+
+export const persistor = persistStore(store);
